18bf6a92477376f7e81b68699a629541ba753daa
Eloi Sanchez <eloisanchez16@gmail.com>
2024-02-19 10:24:16 +0100

    autopair and theme

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,27 +1,27 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
   keys = {
     {
       -- Customize or remove this keymap to your liking
-      "<leader>f",
+      '<leader>f',
       function()
-        require("conform").format({ async = true, lsp_fallback = true })
+        require('conform').format { async = true, lsp_fallback = true }
       end,
-      mode = "",
-      desc = "Format buffer",
+      mode = '',
+      desc = 'Format buffer',
     },
   },
   -- Everything in opts will be passed to setup()
   opts = {
     -- Define your formatters
     formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "black" },
-      yaml = { "prettier", "yamlfmt" },
-      markdown = { "prettier", "markdownlint" },
-      json = { "prettier" },
+      lua = { 'stylua' },
+      python = { 'black' },
+      yaml = { 'prettier' },
+      markdown = { 'prettier', 'markdownlint' },
+      json = { 'prettier' },
     },
     -- Set up format-on-save
     format_on_save = { timeout_ms = 500, lsp_fallback = true },
