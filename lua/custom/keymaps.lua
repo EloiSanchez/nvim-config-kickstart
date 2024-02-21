208f511d2dce4404b7c68e745d998b99966c2f04
Eloi Sanchez <eloisanchez16@gmail.com>
2024-02-21 14:54:33 +0100

    Added vertical terminal keymap and more closing options

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -22,9 +22,12 @@ vim.keymap.set('n', '<leader>t', api_tree.tree.open, { desc = 'Focus Tree' })
 -- open ternimal
 vim.keymap.set('n', '<A-h>', ':ToggleTerm size=12 direction=horizontal<CR>', { desc = 'Toggle Horizontal Terminal' })
 vim.keymap.set('n', '<A-f>', ':ToggleTerm direction=float<CR>', { desc = 'Toggle Floating Terminal' })
+vim.keymap.set('n', '<A-v>', ':ToggleTerm size=100 direction=vertical<CR>', { desc = 'Toggle Floating Terminal' })
 
 -- close terminal
 vim.keymap.set('t', '<A-h>', '<Esc><C-\\><C-n>:ToggleTerm<CR>', { desc = 'Close terminal' })
+vim.keymap.set('t', '<A-v>', '<Esc><C-\\><C-n>:ToggleTerm<CR>', { desc = 'Close terminal' })
+vim.keymap.set('t', '<A-f>', '<Esc><C-\\><C-n>:ToggleTerm<CR>', { desc = 'Close terminal' })
 
 -- toggle terminal for lazygit
 local Terminal = require('toggleterm.terminal').Terminal
